3db67dbc64bdbd287d2e3377095a0c1e5c9eaded
AnjanFF <52600828+AnjanFF@users.noreply.github.com>
2022-05-02 12:37:46 +0200

    Add files via upload

---
+++ opencinema/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "opencinema",
+
+    'summary': """
+        Modulo prueba""",
+
+    'description': """
+        Prueba modulo opencinema
+    """,
+
+    'author': "Angel",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/opencinema.xml',
+    ],
+}
